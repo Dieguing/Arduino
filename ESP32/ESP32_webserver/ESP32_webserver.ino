899bfcb5145907eba148d1f4fe886fdb6ea59456
Diego Fernandez <ingdnfernandez@gmail.com>
2022-06-13 19:44:51 +0200

    Web server configured

--- ESP32/ESP32_webserver/ESP32_webserver.ino
+++ ESP32/ESP32_webserver/ESP32_webserver.ino
@@ -1,15 +1,15 @@
 // PubNub MQTT example using ESP32.
 #include <WiFi.h>
+#include <WebServer.h>
+#include <Ticker.h>
+#include <WebSocketsServer.h>
+
+//Defines
 #define analogPin 34
-#define LED 2
 
-
-// Assign output variables to GPIO pins
-const int output5 = 5;
-const int output4 = 4;
-// Auxiliar variables to store the current output state
-String output5State = "off";
-String output2State = "off";
+Ticker timer;
+bool get_data = false;
+float temp_val;
 
 // WiFi Connection info.
 const char* ssid = "Skynet_2.0";
@@ -17,35 +17,84 @@ const char* password =  "5anT1BrDi";
 const float temps[46]={-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
 const float volts[46]={3.17, 3.07, 2.96, 2.87, 2.77, 2.69, 2.60, 2.52, 2.45, 2.37, 2.31, 2.24, 2.18, 2.12, 2.06, 2.01, 1.96, 1.91, 1.86, 1.82, 1.78, 1.74, 1.70, 1.66, 1.62, 1.59, 1.56, 1.53, 1.50, 1.47, 1.44, 1.42, 1.39, 1.37, 1.34, 1.32, 1.30, 1.28, 1.26, 1.24, 1.23, 1.21, 1.19, 1.18, 1.16, 1.15};
 
+// Running a web server
+WebServer server(80);
+
 // Set web server port number to 80
-WiFiServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
-// Variable to store the HTTP request
-String header;
-float temp_val;
-long lastMsg = 0;
-
-// Current time
-unsigned long currentTime = millis();
-
-// Previous time
-unsigned long previousTime = 0; 
-
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
+// Serving a web page (from flash memory)
+// formatted as a string literal!
+char webpage[] PROGMEM = R"=====(
+<html>
+<!-- Adding a data chart using Chart.js -->
+<head>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js'></script>
+</head>
+<body onload="javascript:init()" style="
+  width: 800px;
+  height: 400px;
+  ">
+<!-- Adding a slider for controlling data rate -->
+<div>
+  <input type="range" min="1" max="10" value="5" id="dataRateSlider" oninput="sendDataRate()" />
+  <label for="dataRateSlider" id="dataRateLabel">Rate: 0.2Hz</label>
+</div>
+<hr />
+<div>
+  <canvas id="line-chart" width="1200" height="700"></canvas>
+</div>
+<!-- Adding a websocket to the client (webpage) -->
+<script>
+  var webSocket, dataPlot;
+  var maxDataPoints = 20;
+  function removeData(){
+    dataPlot.data.labels.shift();
+    dataPlot.data.datasets[0].data.shift();
+  }
+  function addData(label, data) {
+    if(dataPlot.data.labels.length > maxDataPoints) removeData();
+    dataPlot.data.labels.push(label);
+    dataPlot.data.datasets[0].data.push(data);
+    dataPlot.update();
+  }
+  function init() {
+    webSocket = new WebSocket('ws://' + window.location.hostname + ':81/');
+    dataPlot = new Chart(document.getElementById("line-chart"), {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [{
+          data: [-10, -5, 0, 5, 10, 15, 20 ,25, 30, 35, 40, 45],
+          label: "Temperature (C)",
+          borderColor: "#3e95cd",
+          fill: false
+        }]
+      }
+    });
+    webSocket.onmessage = function(event) {
+      var data = JSON.parse(event.data);
+      var today = new Date();
+      var t = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      addData(t, data.value);
+    }
+  }
+  function sendDataRate(){
+    var dataRate = document.getElementById("dataRateSlider").value;
+    webSocket.send(dataRate);
+    dataRate = 1.0/dataRate;
+    document.getElementById("dataRateLabel").innerHTML = "Rate: " + dataRate.toFixed(2) + "Hz";
+  }
+</script>
+</body>
+</html>
+)=====";
 
 void setup() {
   Serial.begin(115200);
-  // Initialize the output variables as outputs
+  // Initialize the output variables as input
   pinMode(analogPin, INPUT);
-   // Initialize the output variables as outputs
-  pinMode(output5, OUTPUT);
-  pinMode(LED, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output5, LOW);
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
+
   
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
@@ -60,121 +109,50 @@ void setup() {
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  server.on("/",[](){
+    server.send_P(200, "text/html", webpage);
+  });
   server.begin();
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  
+  timer.attach(5, getData);
 }
 
 void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
- 
-  if (client) 
-  {       
-  // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /2/on") >= 0) {
-              Serial.println("GPIO 2 on");
-              output2State = "on";
-              digitalWrite(LED, HIGH);
-            } else if (header.indexOf("GET /2/off") >= 0) {
-              Serial.println("GPIO 2 off");
-              output2State = "off";
-              digitalWrite(LED, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 2 on");
-              output5State = "on";
-              digitalWrite(output5, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              output5State = "off";
-              digitalWrite(output5, LOW);
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 2  
-            client.println("<p>GPIO 2 - State " + output2State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output2State=="off") {
-              client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 27  
-            client.println("<p>GPIO 27 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output5State=="off") {
-              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-  
-  long now = millis();
-  if(now - lastMsg > 1000) 
-  {
-     lastMsg = now;
-     temp_val = analogRead(analogPin)*3.83/4095;  // read the input pin
+    
+  webSocket.loop();
+  server.handleClient();
 
+  if(get_data) 
+  {
+     // Serial.println(bmp.readTemperature());
+     String json = "{\"value\":";
+     temp_val = analogRead(analogPin)*3.83/4095;  // read the input pin
      for (int i=0; i<47; i++)
      {
         if (temp_val>volts[i])
         {
           Serial.println(temps[i]);
+          json += temps[i];
           break;
         } 
      }
+     json += "}";
+     webSocket.broadcastTXT(json.c_str(), json.length());
+     get_data = false;
+  }
+}
+
+void getData() {
+  get_data = true;
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length){
+  // Do something with the data from the client
+  if(type == WStype_TEXT){
+    float dataRate = (float) atof((const char *) &payload[0]);
+    timer.detach();
+    timer.attach(dataRate, getData);
   }
 }
